b3accd009ce6f5c1806a6942f67b0e12a2d32c0a
muqsith <01muqsith@gmail.com>
2023-12-29 11:15:08 +0700

    test

--- handler.js
+++ handler.js
@@ -68,70 +68,71 @@ export default async function (sock, m) {
         case 'digi':
           {
             if (who == '6289649178812@s.whatsapp.net') {
-              //             let refId = makeid(7);
-              //             const apiUrl = 'https://api.digiflazz.com/v1/transaction';
-              //             const buyerSkuCode = m.args[0]; // Replace this with the product code
-              //             const customerNo = m.args[1]; // Replace this with the customer's phone number
+              let refId = makeid(7);
+              const apiUrl = 'https://api.digiflazz.com/v1/transaction';
+              const buyerSkuCode = m.args[0]; // Replace this with the product code
+              const customerNo = m.args[1]; // Replace this with the customer's phone number
 
-              //             // Calculate the signature using the specified formula: md5(username + apiKey + ref_id)
-              //             const signature = crypto
-              //               .createHash('md5')
-              //               .update(username + process.env.APIKEY + refId)
-              //               .digest('hex');
+              // Calculate the signature using the specified formula: md5(username + apiKey + ref_id)
+              const signature = crypto
+                .createHash('md5')
+                .update(username + process.env.APIKEY + refId)
+                .digest('hex');
 
-              //             // Prepare the request body for initiating the transaction
-              //             const makeRequestBody = {
-              //               username: username,
-              //               buyer_sku_code: buyerSkuCode,
-              //               customer_no: customerNo,
-              //               ref_id: process.env.APIKEY,
-              //               sign: signature,
-              //             };
-              //             reply(`*TUNGGU SEBENTAR*`);
-              //             function checkTransactionStatus() {
-              //               // Make the POST request to initiate the transaction
-              //               fetch(apiUrl, {
-              //                 method: 'POST',
-              //                 headers: {
-              //                   'Content-Type': 'application/json',
-              //                 },
-              //                 body: JSON.stringify(makeRequestBody),
-              //               })
-              //                 .then((response) => response.json())
-              //                 .then((data) => {
-              //                   const status = data.data.status;
-              //                   let balas = `
-              // ┏━━ꕥ *「 DETAIL ORDERAN 」* ꕥ━⬣
-              // ┃> *ID GAME:* ${data.data.customer_no}
-              // ┃> *PRODUK:* ${data.data.buyer_sku_code}
-              // ┃> *SN:* ${data.data.sn}
-              // ┃> *STATUS:* ${data.data.message}
-              // ┃> *Ref_Id:* ${data.data.ref_id}
-              // ┃> *RC STATUS:* ${data.data.rc}
-              // ┗━━━━━━━━━━━━━━━━━━━ꕥ`;
+              // Prepare the request body for initiating the transaction
+              const makeRequestBody = {
+                username: username,
+                buyer_sku_code: buyerSkuCode,
+                customer_no: customerNo,
+                ref_id: process.env.APIKEY,
+                sign: signature,
+              };
+              reply(`*TUNGGU SEBENTAR YAK*`);
+              function checkTransactionStatus() {
+                // Make the POST request to initiate the transaction
+                fetch(apiUrl, {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify(makeRequestBody),
+                })
+                  .then((response) => response.json())
+                  .then((data) => {
+                    const status = data.data.status;
+                    let balas = `
+              ┏━━ꕥ *「 DETAIL ORDERAN 」* ꕥ━⬣
+              ┃> *ID GAME:* ${data.data.customer_no}
+              ┃> *PRODUK:* ${data.data.buyer_sku_code}
+              ┃> *SN:* ${data.data.sn}
+              ┃> *STATUS:* ${data.data.message}
+              ┃> *Ref_Id:* ${data.data.ref_id}
+              ┃> *RC STATUS:* ${data.data.rc}
+              ┗━━━━━━━━━━━━━━━━━━━ꕥ`;
 
-              //                   if (status === 'Pending') {
-              //                     // Wait for a few seconds before checking the status again
-              //                     setTimeout(() => {
-              //                       checkTransactionStatus(); // Call the function again to check the status
-              //                     }, 5000);
-              //                   } else if (status === 'Gagal') {
-              //                     reply(`*Transaction failed.* ${data.data.message}`);
-              //                   } else {
-              //                     // If the status is not 'Pending' or 'Failed', set the reply
-              //                     reply(balas);
-              //                   }
-              //                 })
-              //                 .catch((error) => {
-              //                   // Handle any errors that occur during the API request
-              //                   console.error('Error:', error);
-              //                   reply('Gagal memproses permintaan, silakan coba lagi nanti.');
-              //                 });
-              //             }
+                    if (status === 'Pending') {
+                      // Wait for a few seconds before checking the status again
+                      setTimeout(() => {
+                        checkTransactionStatus(); // Call the function again to check the status
+                      }, 5000);
+                    } else if (status === 'Gagal') {
+                      reply(`*Transaction failed.* ${data.data.message}`);
+                    } else {
+                      // If the status is not 'Pending' or 'Failed', set the reply
+                      reply(balas);
+                    }
+                  })
+                  .catch((error) => {
+                    // Handle any errors that occur during the API request
+                    console.error('Error:', error);
+                    reply(
+                      'Gagal memproses permintaan, silakan coba lagi nanti.',
+                    );
+                  });
+              }
 
-              //             // Call the function to initiate the API request and check the status
-              //             checkTransactionStatus();
-              reply('sukses');
+              // Call the function to initiate the API request and check the status
+              checkTransactionStatus();
             } else {
               let penyusub = m.key.participant.split('@')[0];
               var kirimke = '6289649178812@s.whatsapp.net';
